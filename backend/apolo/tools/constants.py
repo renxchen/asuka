2490548e8abef67f9bafb57558d377c843359254
yangyuan <yangyuan@cisco.com>
2017-12-25 10:36:01 +0800

    add name and icon informations of policy tree node

--- backend/apolo/tools/constants.py
+++ backend/apolo/tools/constants.py
@@ -59,3 +59,20 @@ SPLIT_RULE_SLASH = 'slash'
 SPLIT_RULE_OTHER = 'other'
 INSTEAD = '@@'
 NO_MATCH_EXTRACT_DATA_REGEXP = 'Can not match the provided regular Expression.'
+
+#########################################
+# Policy Tree Node's information
+# Rule Tree Node's information
+#########################################
+BLOCK_RULE_TREE_KIND_ONE_NAME ='b1'    # インデントによって絞る
+BLOCK_RULE_TREE_KIND_TWO_NAME = 'b2'   #  行数によって絞る
+BLOCK_RULE_TREE_KIND_THREE_NAME ='b3'  #  指定文字列の間
+DATA_RULE_TREE_KIND_ONE_NAME = 'd1'    #  特定文字からの距離
+DATA_RULE_TREE_KIND_TWO_NAME = 'd2'    #  行数指定
+DATA_RULE_TREE_KIND_THREE_NAME ='d3'   #  正規表現
+POLICY_TREE_ROOT_ICON = 'pcy_root_icon.jpg'
+BLOCK_NODE_ICON = 'rule_block_node_icon.jpg'
+DATA_NODE_ICON = 'rule_data_node_icon.jpg'
+RULE_NODE_ICON = 'rule_tree_node_icon.jpg'
+
+
