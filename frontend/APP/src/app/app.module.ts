a844c1b3084124e14a9468f7d5c09b77481a35ef
kimli <kimli@cisco.com>
2017-12-29 17:23:35 +0800

    add frontend and add ignore about frontend

---
+++ frontend/APP/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { appRouting,  } from './app.router';
+import { HttpModule, Http } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '../components/utils/httpClient.module';
+import { BreadCrumbModule } from '../components/breadCrumb/bread-crumb.module';
+import { HttpClientComponent } from '../components/utils/httpClient';
+import { LoginComponentModule } from './login/login.module';
+import { IndexComponentModule } from './index/index.module';
+import { CPViewComponentModule } from './collectionPolicy/collectionPolicy.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    appRouting,
+    HttpClientModule,
+    HttpModule,
+    LoginComponentModule,
+    IndexComponentModule,
+    CPViewComponentModule,
+    BreadCrumbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
