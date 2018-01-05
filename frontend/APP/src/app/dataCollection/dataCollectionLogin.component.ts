019c0be31482a73115d403f3dc6deca1d3511d63
necwang <necwang@cisco.com>
2018-01-05 17:14:11 +0800

    20180105 necwang Finish dc login page basic design

--- frontend/APP/src/app/dataCollection/dataCollectionLogin.component.ts
+++ frontend/APP/src/app/dataCollection/dataCollectionLogin.component.ts
@@ -1,25 +1,100 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, } from '@angular/core';
 import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { HttpClientComponent } from '../../components/utils/httpClient';
 
 @Component({
-    selector: 'modal-content',
+    selector: 'dc-login',
     templateUrl: 'dataCollectionLogin.component.html',
     styleUrls: ['dataCollection.component.less']
 })
 
-export class DataCollectionLoginComponent implements OnInit {
+export class DataCollectionLoginComponent implements OnInit, AfterViewInit {
     title: string;
-    id: number = 0;
-    // modalTitle:string;
+    id: number = -1;
+    selectedOsType: string;
+    osType: any = [];
+    priority: any;
+    priorities: any = [{id: 0, value: '標準'}, {id:1, value: '高'}];
+    deviceGroup: any;
+    deviceGroups: any = [];
+    policyGroup: any;
+    policyGroups:any = [];
+    validPeriodTypes: any = [{id:0, value:'期間なし'}, {id:1, value: '期間あり'}];
+    dataScheduleTypes: any = [{id:0, value:'常に取得'}, {id:1, value: '取得停止'}, {id:2, value: '周期取得'}];
+    weekdays: any = [
+        {id: 1, value: '月', ifCheck: 'checked'},
+        {id: 2, value: '火', ifCheck: ''},
+        {id: 3, value: '水', ifCheck: ''},
+        {id: 4, value: '木', ifCheck: ''},
+        {id: 5, value: '金', ifCheck: 'checked'},
+        {id: 6, value: '土', ifCheck: ''},
+        {id: 7, value: '日', ifCheck: ''}
+    ];
 
-    constructor(public bsModalRef: BsModalRef) {}
+    // modalTitle:string;
+    constructor(
+        private router: Router,
+        private activeRoute: ActivatedRoute,
+        private httpClient: HttpClientComponent,
+        public bsModalRef: BsModalRef
+    ) { }
 
     ngOnInit() {
-        // this.modalTitle = 'data';
-        // this.cpType = 'cli';
-        // this.modalRef = this.modalService;
-        // this.drawCPTable();
+        this.getOsTypes();
+
     }
 
+    ngAfterViewInit() {
+        setTimeout(() => {
+           if(this.id == -1){
+                this.setInitSelect();
+                $('#validPeriod').hide();
+                $('#dataSchedule').hide();
+            } else {
+                //set init value for this id
+            }
+        },0);
+    }
+
+    getOsTypes() {
+
+    }
+
+    setInitSelect() {
+        $('input[value="0"]').attr('checked',true);
+    }
+
+    changeOsType() {
+        let _p = $('#osType').val();
+        // send this os type id to API and get groups information
+        let _this = this;
+        _this.deviceGroups = [];
+        _this.policyGroups = [];
+    }
+
+    changeValidPeriodType(id: number){
+        if (id == 1){
+            $('#validPeriod').show();
+        } else {
+            $('#validPeriod').hide();
+        }
+    }
+
+    changeDataScheduleType(id: number){
+        if (id == 2){
+            $('#dataSchedule').show();
+        } else {
+            $('#dataSchedule').hide();
+        }
+    }
+
+    save() {
+        if (this.id == -1){
+            // save as new
+        } else {
+            // save
+        }
+    }
 
 }
