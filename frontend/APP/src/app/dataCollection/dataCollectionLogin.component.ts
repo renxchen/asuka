cbf65992ab6d1c8333ff87d309ae5b053e38f10a
necwang <necwang@cisco.com>
2018-01-19 17:12:06 +0800

    20180119 necwang Update new schedule adding and schedule loading with background

--- frontend/APP/src/app/dataCollection/dataCollectionLogin.component.ts
+++ frontend/APP/src/app/dataCollection/dataCollectionLogin.component.ts
@@ -14,30 +14,39 @@ enableProdMode();
 })
 
 export class DataCollectionLoginComponent implements OnInit, AfterViewInit {
-    apiPrefix: any;
+    apiPrefix: any = '/v1';
     title: string;
     id: number = -1;
     // minDate = new Date(2017, 5, 10);
-    maxDate = new Date(2018, 10, 15);
+    // maxDate = new Date(2018, 10, 15);
     bsValue: Date = new Date();
-
     isTimeMeridian: boolean = false;
+
+    startDateTime: Date = new Date(2018, 0, 1, 0, 0);
+    endDateTime: Date = new Date(2018, 0, 1, 23, 59);
+
     startTime: Date = new Date(2018, 1, 1, 0, 0);
     endTime: Date = new Date(2018, 1, 1, 23, 59);
-    startDateTime: Date = new Date(2018, 1, 1, 0, 0);
-    endDateTime: Date = new Date(2018, 1, 1, 23, 59);
+
     // bsRangeValue: any = [new Date(2017, 7, 4), new Date(2017, 7, 20)];
 
-    selectedOsType: String;
-    osType: any = [{id: 0, name: 'os type 1'}, {id:1, name: 'os type 2'}];
+    selectedOsType: string;
+    osType: any = [];
+
     priority: any;
-    priorities: any = [{id: 0, value: '標準'}, {id:1, value: '高'}];
+    priorities: any = [{id: 0, value: '標準'}, {id: 1, value: '高'}];
+
     deviceGroup: any;
     deviceGroups: any = [];
+
     policyGroup: any;
     policyGroups:any = [];
-    validPeriodTypes: any = [{id:0, value:'期間なし'}, {id:1, value: '期間あり'}];
-    dataScheduleTypes: any = [{id:0, value:'常に取得'}, {id:1, value: '取得停止'}, {id:2, value: '周期取得'}];
+
+    validPeriodType:any;
+    validPeriodTypes: any = [{id: 0, value:'期間なし'}, {id: 1, value: '期間あり'}];
+
+    dataScheduleType: any;
+    dataScheduleTypes: any = [{id: 0, value:'常に取得'}, {id: 1, value: '取得停止'}, {id: 2, value: '周期取得'}];
     weekdays: any = [
         {id: 1, value: '月', ifCheck: 'checked'},
         {id: 2, value: '火', ifCheck: 'checked'},
@@ -57,22 +66,20 @@ export class DataCollectionLoginComponent implements OnInit, AfterViewInit {
     ) { }
 
     ngOnInit() {
-        this.getOsTypes();
-        this.deviceGroups = [{id: 0, name: 'device group 1'}, {id:1, name: 'device group 2'}];
-        this.policyGroups = [{id: 0, name: 'cpg 1'}, {id:1, name: 'cpg 2'}];
+        this.httpClient.setUrl(this.apiPrefix);
 
     }
 
     ngAfterViewInit() {
         setTimeout(() => {
            if(this.id == -1){
-
+                this.getOsTypes();
                 this.setInitSelect();
                 $('button.btn-danger').hide();
                 $('#validPeriod').hide();
                 $('#dataSchedule').hide();
             } else {
-               // this.getDetailById(this.id);
+                this.getOsTypes();
                 //set init value for this id
                // $('button.btn-primary').attr('disabled','disabled');
             }
@@ -80,21 +87,78 @@ export class DataCollectionLoginComponent implements OnInit, AfterViewInit {
     }
 
     getOsTypes() {
+        this.httpClient
+            .toJson(this.httpClient.get('/api_new_data_collection/new/'))
+            .subscribe(res => {
+                if (res['status'] && res['status']['status'].toLowerCase() === 'true') {
+                    if (res['data']) {
+                        this.osType = res['data'];
+                        this.selectedOsType = this.osType[0]['ostypeid'].toString();
+                        this.getGroups();
+
+                    }
+                } else {
+                    if (res['status'] && res['status']['message']) {
+                        alert(res['status']['message']);
+                    }
+                }
+            });
+    }
+
+    getGroups() {
+        this.httpClient
+            .toJson(this.httpClient.get('/api_new_data_collection/?ostype_id='+this.selectedOsType))
+            .subscribe(res => {
+                if (res['status'] && res['status']['status'].toLowerCase() === 'true') {
+                    // if (res['data']) {
+                        this.deviceGroups = res['device_groups'];
+                        this.policyGroups = res['cp_groups'];
+                        if (this.id == -1) {
+                            this.deviceGroup = this.deviceGroups[0]['group_id'].toString();
+                            this.policyGroup = '-1';
+                        } else {
+                            this.getDetailById(this.id);
+                        }
+
+                    // }
+                } else {
+                    if (res['status'] && res['status']['message']) {
+                        alert(res['status']['message']);
+                    }
+                }
+            });
+        // this.deviceGroups = [{id: 0, name: 'device group 1'}, {id:1, name: 'device group 2'}];
+        // this.policyGroups = [{id: 0, name: 'cpg 1'}, {id:1, name: 'cpg 2'}];
+
 
     }
 
     getDetailById(id){
         console.log("schedule id "+id);
-        this.apiPrefix = '/v1';
         let url = '/api_data_collection/?id=' + id;
-        this.httpClient.setUrl(this.apiPrefix);
-        // return this.httpClient.toJson(this.httpClient.get(url));
         this.httpClient.toJson(this.httpClient.get(url)).subscribe(res => {
-            let data = res['data'];
-            console.log(res.data);
-            this.selectedOsType = _.get(data, 'ostype_name').toString();
-            // res.data["ostype_name"];
-            console.log(this.selectedOsType);
+            if (res['status'] && res['status']['status'].toLowerCase() === 'true') {
+                    if (res['data']) {
+                        let data = res['data'];
+                        console.log(res.data);
+                        this.id = _.get(data, 'schedule_id');
+                        this.selectedOsType = _.get(data, 'ostype_id').toString();
+                        this.priority = _.get(data, 'priority');
+                        this.deviceGroup = _.get(data, 'device_group_id').toString();
+                        this.policyGroup = _.get(data, 'policy_group_id').toString();
+
+                    }
+                } else {
+                    if (res['status'] && res['status']['message']) {
+                        alert(res['status']['message']);
+                    }
+                }
+
+            // scheduleInfo['valid_period_type'] = this.validPeriodType.toString();
+            // scheduleInfo['start_period_time'] = startPeriodTime;
+            // scheduleInfo['end_period_time'] = endPeriodTime;
+            // scheduleInfo['data_schedule_type'] = this.dataScheduleType.toString();
+            // scheduleInfo['data_schedule_time'] = dataScheduleTime;
 
         });
         // console.log(result);
@@ -102,17 +166,17 @@ export class DataCollectionLoginComponent implements OnInit, AfterViewInit {
 
     setInitSelect() {
         $('input[value="0"]').attr('checked','checked');
+        this.dataScheduleType = 0;
+        this.validPeriodType = 0;
+        this.priority = 0;
     }
 
     changeOsType() {
-        let _p = $('#osType').val();
-        // send this os type id to API and get groups information
-        let _this = this;
-        _this.deviceGroups = [{id: 0, name: 'device group 1'}, {id:1, name: 'device group 2'}];
-        _this.policyGroups = [{id: 0, name: 'cpg 1'}, {id:1, name: 'cpg 2'}];
+        this.getGroups();
     }
 
     changeValidPeriodType(id: number){
+        this.validPeriodType = id;
         if (id == 1){
             $('#validPeriod').show();
         } else {
@@ -121,6 +185,7 @@ export class DataCollectionLoginComponent implements OnInit, AfterViewInit {
     }
 
     changeDataScheduleType(id: number){
+        this.dataScheduleType = id;
         if (id == 2){
             $('#dataSchedule').show();
         } else {
@@ -128,42 +193,122 @@ export class DataCollectionLoginComponent implements OnInit, AfterViewInit {
         }
     }
 
+    changeWeekdays(id) {
+        if (this.weekdays[id-1]['ifCheck'] == 'checked'){
+            this.weekdays[id-1]['ifCheck'] = '';
+        } else {
+            this.weekdays[id-1]['ifCheck'] = 'checked';
+        }
+
+    }
+
+    filledToTwoNumber(num){
+        if (num < 10){
+            num = "0" + num.toString();
+        }
+        return num
+    }
+
     save() {
         let startDate = $('input[name="startDate"]').val();
-        // console.log(startDate);
+        let startPeriodTime = startDate+'@'
+            +this.filledToTwoNumber(this.startDateTime.getHours())+':'
+            +this.filledToTwoNumber(this.startDateTime.getMinutes());
         let endDate = $('input[name="endDate"]').val();
-        // console.log(endDate);
-        let timeFormat = this.startTime.getHours();
-        console.log(timeFormat);
-        let timeFormat2 = this.startTime.getMinutes();
-        console.log(timeFormat2);
-        if (this.id == -1){
-
-        } else {
-            // save
+        let endPeriodTime = endDate+'@'
+            +this.filledToTwoNumber(this.endDateTime.getHours())+':'
+            +this.filledToTwoNumber(this.endDateTime.getMinutes());
+        let dataScheduleStartTime = this.filledToTwoNumber(this.startTime.getHours())
+            +':'+this.filledToTwoNumber(this.startTime.getMinutes());
+        let dataScheduleEndTime = this.filledToTwoNumber(this.endTime.getHours())
+            +':'+this.filledToTwoNumber(this.endTime.getMinutes());
+        let dataScheduleTime = '';
+        for (let i=0; i<this.weekdays.length; i++){
+            if(this.weekdays[i]['ifCheck'] == 'checked'){
+                dataScheduleTime += this.weekdays[i]['id']+';';
+            }
         }
-        let ostype = $('#osType').val();
-        console.log('os',ostype);
-        console.log(this.selectedOsType);
+        dataScheduleTime = dataScheduleTime.substring(0,dataScheduleTime.length-1)+'@'+dataScheduleStartTime+'-'+dataScheduleEndTime;
+
+        // let url = '/api_collection_policy/?policy_type=' + parseInt(this.cPType, 0);
+        if (this.doCheck()) {
+            let scheduleInfo: any = {};
+            let url = '/api_new_data_collection/';
+
+            scheduleInfo['ostype_id'] = this.selectedOsType;
+            scheduleInfo['priority'] = this.priority.toString();
+            scheduleInfo['device_group_id'] = this.deviceGroup;
+            scheduleInfo['policy_group_id'] = this.policyGroup;
+            scheduleInfo['valid_period_type'] = this.validPeriodType.toString();
+            scheduleInfo['start_period_time'] = startPeriodTime;
+            scheduleInfo['end_period_time'] = endPeriodTime;
+            scheduleInfo['data_schedule_type'] = this.dataScheduleType.toString();
+            scheduleInfo['data_schedule_time'] = dataScheduleTime;
+            scheduleInfo['period_time'] = '';
+            scheduleInfo['weeks'] = '';
+
+            console.log(scheduleInfo);
+            // return;
+            if (this.id == -1){
+                this.httpClient
+                .toJson(this.httpClient.post(url, scheduleInfo))
+                .subscribe(res => {
+                    if (res['status']['status'].toString().toLowerCase() === 'true') {
+                        if (res['data']) {
+                            // let id = res['data']['coll_policy_id'];
+                            // this.router.navigate(['/index/cliCPEdit'],
+                            //     { queryParams: { 'id': id } });
+                        }
+                    } else {
+                        // if (res['status'] && res['status']['message'] === 'CP_NAME_DUPLICATE') {
+                        //     this.uniqueFlg = false;
+                        // } else {
+                        //     alert(res['status']['message']);
+                        // }
+                    }
+                });
+            } else {
+                scheduleInfo['schedule_id'] = this.id.toString();
+                this.httpClient
+                .toJson(this.httpClient.put(url, scheduleInfo))
+                .subscribe(res => {
+                    if (res['status']['status'].toString().toLowerCase() === 'true') {
+                        if (res['data']) {
+                            // let id = res['data']['coll_policy_id'];
+                            // this.router.navigate(['/index/cliCPEdit'],
+                            //     { queryParams: { 'id': id } });
+                        }
+                    } else {
+                        // if (res['status'] && res['status']['message'] === 'CP_NAME_DUPLICATE') {
+                        //     this.uniqueFlg = false;
+                        // } else {
+                        //     alert(res['status']['message']);
+                        // }
+                    }
+                });
+            }
+
+
+        }
+
+
+    }
+    doCheck(){
+        let flag: boolean = true;
+        return flag;
+
     }
 
     delete() {
         let isDelete: boolean = confirm('delete?');
         if (isDelete){
-            let _id = this.id;
-            console.log(_id);
-            this.apiPrefix = '/v1';
-            let url = '/api_data_collection/?id=' + _id;
-            this.httpClient.setUrl(this.apiPrefix);
-            // return this.httpClient.toJson(this.httpClient.get(url));
+            let url = '/api_data_collection/?id=' + this.id;
             this.httpClient
                 .toJson(this.httpClient.delete(url))
                 .subscribe(res => {
-            console.log('delete',res);
-        });
-
+                    console.log('delete',res);
+                });
         }
-
     }
 
 }
