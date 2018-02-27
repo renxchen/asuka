a204637def236803f013574835ed2ef2e012d60b
necwang <necwang@cisco.com>
2018-02-27 13:30:49 +0800

    20180227 necwang Update draft of action policy view page

--- frontend/APP/src/app/actionPolicy/dataTableView.component.ts
+++ frontend/APP/src/app/actionPolicy/dataTableView.component.ts
@@ -13,6 +13,20 @@ import * as _ from 'lodash';
 })
 export class DataTableViewComponent implements OnInit, AfterViewInit {
 
+    tableModel: any = [
+        {label: 'No', hidden: true, name: 'table_id', index: 'table_id'},
+        {label: 'テーブル名',  name: 'name', width: 30, align: 'center'},
+        {label: '概要', name: 'description', width: 45, align: 'center'},
+        {label: 'アクション', name: 'action', width: 30, align: 'center', search: false,
+        formatter: this.formatterButtons},
+    ];
+
+    testData: any = [
+        {table_id: 10, name: 'data table 1', description: 'description of data table 1'},
+        {table_id: 11, name: 'data table 2', description: 'description of data table 2'},
+        {table_id: 12, name: 'data table 3', description: 'description of data table 3'},
+    ];
+
     constructor(
         private modalService: BsModalService,
         public httpClient: HttpClientComponent,
@@ -26,6 +40,52 @@ export class DataTableViewComponent implements OnInit, AfterViewInit {
     }
 
     ngAfterViewInit() {
+        this.drawDCTable();
+
+    }
+
+    formatterButtons(cellvalue, options, rowObject) {
+        // let buttons = '';
+        let buttons = '<button class="btn btn-xs btn-default showInfo" id="show_'+ rowObject["table_id"] + '"><i class="fa fa-info-circle"></i> 表示</button>&nbsp;';
+        buttons += '<button class="btn btn-xs btn-warning delete" id="delete_'+ rowObject["table_id"] + '"><i class="fa fa-minus-square"></i> 削除</button>&nbsp;';
+        buttons += '<button class="btn btn-xs btn-primary addAction" id="add_'+ rowObject["table_id"] + '"><i class="fa fa-plus-square"></i> アクションポリシー追加</button>';
+        return buttons
+    }
+
+    public drawDCTable() {
+        let _this = this;
+        $('#tableTable').jqGrid({
+            // url: '/v1/api_data_collection/',
+            // datatype: 'JSON',
+            datatype: 'local',
+            // mtype: 'get',
+            colModel: _this.tableModel,
+            // postData: { '': '' },
+            data: _this.testData,
+            // viewrecords: true,
+            loadComplete: function() {
+                // _this.editDC();
+                // _this.renderColor();
+            },
+            rowNum: 10,
+            rowList: [ 10, 20, 30],
+            autowidth: true,
+            beforeSelectRow: function(rowid, e) { return false; },
+            height: 230,
+            pager: '#tablePager',
+            // jsonReader: {
+            //     root: 'data',
+            //     page: 'current_page_num',
+            //     total: 'num_page',
+            //     records: 'total_num',
+            //     userData: 'status',
+            //     repeatitems: false,
+            // },
+        });
+        $('#tableTable').jqGrid('filterToolbar', {defaultSearch: 'cn'});
+    }
+
+    newTable(){
 
     }
 }
