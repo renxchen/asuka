4bf65dbdc69e37a458cfd3eebd12efefae1eb871
kimli <kimli@cisco.com>
2018-03-02 17:14:00 +0800

    add coulumA,B verify and column related api

--- backend/apolo/apolomgr/resource/action_policy/action_policy_column_views.py
+++ backend/apolo/apolomgr/resource/action_policy/action_policy_column_views.py
@@ -10,21 +10,16 @@
 
 """
 import traceback
-import importlib
 from rest_framework import viewsets
-from django.utils.translation import gettext
 from django.core.paginator import Paginator
 from backend.apolo.tools.exception import exception_handler
 from backend.apolo.tools.views_helper import api_return
 from backend.apolo.tools import constants
 from backend.apolo.tools import views_helper
-from backend.apolo.models import Triggers, TriggerDetail, DataTableItems
-from django.db import transaction
-from backend.apolo.serializer.action_policy_serializer import TriggerSerializer, ActionsSerializer, \
-    TriggerDetailSerializer
-import time
-import simplejson as json
+from backend.apolo.models import DataTable
 import logging
+from backend.apolo.apolomgr.resource.action_policy_table.data_table_step1_views import TableViewsSet
+from backend.apolo.apolomgr.resource.common.common_policy_tree.policy_tree import Policy_tree
 
 
 class ActionPolicyColumnViewSet(viewsets.ViewSet):
@@ -41,42 +36,63 @@ class ActionPolicyColumnViewSet(viewsets.ViewSet):
         if request.method.lower() == 'post' or request.method.lower() == 'put':
             method = 'BODY'
         self.table_id = views_helper.get_request_value(self.request, 'id', method)
+        self.policy_id = views_helper.get_request_value(self.request, 'policy_id', method)
 
     def get(self):
         try:
-            pass
+            if self.table_id is not '':
+                tvs = TableViewsSet(request=self.request)
+                data_history = tvs.get_info_by_table_id(self.table_id)
+                # get tree information
+                pt = Policy_tree(self.policy_id)
+                pt_data = pt.get_policy_tree()
+                data = {
+                    'data': {
+                        'table_name': DataTable.objects.get(table_id=self.table_id).name,
+                        'table_id': DataTable.objects.get(table_id=self.table_id).table_id,
+                        'data_history': data_history,
+                        'policy_tree': pt_data,
+                    },
+                    'new_token': self.new_token,
+                    constants.STATUS: {
+                        constants.STATUS: constants.TRUE,
+                        constants.MESSAGE: constants.SUCCESS
+                    },
+                }
+                return api_return(data=data)
+            dt_query = DataTable.objects.all()
+            dt = dt_query.values('table_id', 'name', 'coll_policy__name', 'descr', 'groups', 'tree', 'coll_policy')
+            data_all = []
+            for per_dt in dt:
+                result = {
+                    'name': per_dt['name'],
+                    'table_id': per_dt['table_id'],
+                    'group_id': per_dt['groups'],
+                    'tree_id': per_dt['tree'],
+                    'policy_name': per_dt['coll_policy__name'],
+                    'policy_id': per_dt['coll_policy'],
+                    'desc': per_dt['descr']
+                }
+                data_all.append(result)
+            paginator = Paginator(data_all, int(self.max_size_per_page))
+            contacts = paginator.page(int(self.page_from))
+            data = {
+                'data': {
+                    'data': contacts.object_list,
+                },
+                'new_token': self.new_token,
+                'num_page': paginator.num_pages,
+                'page_range': list(paginator.page_range),
+                'page_has_next': contacts.has_next(),
+                'total_num': len(contacts.object_list),
+                'current_page_num': contacts.number,
+                constants.STATUS: {
+                    constants.STATUS: constants.TRUE,
+                    constants.MESSAGE: constants.SUCCESS
+                },
+            }
+            return api_return(data=data)
         except Exception, e:
-            if constants.DEBUG_FLAG:
-                print traceback.format_exc(e)
-            return exception_handler(e)
-
-    def post(self):
-        try:
-            with transaction.atomic():
-                pass
-        except Exception, e:
-            transaction.rollback()
-            if True:
-                print traceback.format_exc(e)
-            self.logger.error(e)
-            return exception_handler(e)
-
-    def put(self):
-        try:
-            with transaction.atomic():
-                pass
-        except Exception, e:
-            transaction.rollback()
-            if constants.DEBUG_FLAG:
-                print traceback.format_exc(e)
-            return exception_handler(e)
-
-    def delete(self):
-        try:
-            with transaction.atomic():
-                pass
-        except Exception, e:
-            transaction.rollback()
             if constants.DEBUG_FLAG:
                 print traceback.format_exc(e)
             return exception_handler(e)
--- backend/server/urls.py
+++ backend/server/urls.py
@@ -49,6 +49,8 @@ urlpatterns = [
     url(r'^v1/api_data_table_step1/', resource_api.api_data_table_step1),
     url(r'^v1/api_action_policy/', resource_api.api_action_policy),
     url(r'^v1/api_regenerate_trigger_detail/', resource_api.api_regenerate_trigger_detail),
+    url(r'^v1/api_column/', resource_api.api_column),
+    url(r'^v1/api_column_verify/', resource_api.api_column_verify),
     # action policy related end
     url(r'^v1/api_data_collection_devices/', resource_api.api_data_collection_by_device),
     url(r"^v1/api_collection_policy_edit_page/", resource_api.api_collection_policy_edit),
