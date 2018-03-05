b4861c7fd6740e7b7ecb6d57f53f86bfa3f72556
yangyuan <yangyuan@cisco.com>
2018-03-05 13:42:45 +0800

    modify common_policy_tree/tool.py

--- backend/apolo/apolomgr/resource/common/common_policy_tree/tool.py
+++ backend/apolo/apolomgr/resource/common/common_policy_tree/tool.py
@@ -140,67 +140,67 @@ class Tool(object):
 
     @staticmethod
     def get_data_from_collection_server():
-        # now_time = int(time.time())
-        # json_data = json.dumps({"now_time": now_time, "item_type": -1})
-        # response = requests.post(constants.DATA_COLLECTION_POST_URL, data=json_data)
-        # return response.json()
-        test_json = {
-            "status": "success",
-            "items": [{
-                "policy_group_name": "snmp_test",
-                "policy_group_id": 3,
-                "priority": 1,
-                "item_type": 1,
-                "valid_status": True,
-                "item_id": 5,
-                "coll_policy_id": 5,
-                "device_name": "test device name1",
-                "device_id": 5,
-                "policy_name": "cd 3",
-                "exec_interval": 300
-            },
-                {
-                    "policy_group_name": "cli_test",
-                    "policy_group_id": 1,
-                    "priority": 0,
-                    "item_type": 0,
-                    "valid_status": True,
-                    "item_id": 4,
-                    "coll_policy_id": 5,
-                    "device_name": "test device name3",
-                    "device_id": 3,
-                    "policy_name": "cd 3",
-                    "exec_interval": 300
-                },
-                {
-                    "policy_group_name": "cli_test1",
-                    "policy_group_id": 1,
-                    "priority": 1,
-                    "item_type": 0,
-                    "valid_status": False,
-                    "item_id": 6,
-                    "coll_policy_id": 5,
-                    "device_name": "iso",
-                    "device_id": 6,
-                    "policy_name": "policy test 3",
-                    "exec_interval": 60
-                },
-                {
-                    "policy_group_name": "cli_test1",
-                    "policy_group_id": 1,
-                    "priority": 1,
-                    "item_type": 0,
-                    "valid_status": False,
-                    "item_id": 6,
-                    "coll_policy_id": 6,
-                    "device_name": "cli",
-                    "device_id": 4,
-                    "policy_name": "policy test 1",
-                    "exec_interval": 30
-                },
-            ],
-            "message": ""
-        }
+        now_time = int(time.time())
+        json_data = json.dumps({"now_time": now_time, "item_type": -1})
+        response = requests.post(constants.DATA_COLLECTION_POST_URL, data=json_data)
+        return response.json()
+        # test_json = {
+        #     "status": "success",
+        #     "items": [{
+        #         "policy_group_name": "snmp_test",
+        #         "policy_group_id": 3,
+        #         "priority": 1,
+        #         "item_type": 1,
+        #         "valid_status": True,
+        #         "item_id": 5,
+        #         "coll_policy_id": 5,
+        #         "device_name": "test device name1",
+        #         "device_id": 5,
+        #         "policy_name": "cd 3",
+        #         "exec_interval": 300
+        #     },
+        #         {
+        #             "policy_group_name": "cli_test",
+        #             "policy_group_id": 1,
+        #             "priority": 0,
+        #             "item_type": 0,
+        #             "valid_status": True,
+        #             "item_id": 4,
+        #             "coll_policy_id": 5,
+        #             "device_name": "test device name3",
+        #             "device_id": 3,
+        #             "policy_name": "cd 3",
+        #             "exec_interval": 300
+        #         },
+        #         {
+        #             "policy_group_name": "cli_test1",
+        #             "policy_group_id": 1,
+        #             "priority": 1,
+        #             "item_type": 0,
+        #             "valid_status": False,
+        #             "item_id": 6,
+        #             "coll_policy_id": 5,
+        #             "device_name": "iso",
+        #             "device_id": 6,
+        #             "policy_name": "policy test 3",
+        #             "exec_interval": 60
+        #         },
+        #         {
+        #             "policy_group_name": "cli_test1",
+        #             "policy_group_id": 1,
+        #             "priority": 1,
+        #             "item_type": 0,
+        #             "valid_status": False,
+        #             "item_id": 6,
+        #             "coll_policy_id": 6,
+        #             "device_name": "cli",
+        #             "device_id": 4,
+        #             "policy_name": "policy test 1",
+        #             "exec_interval": 30
+        #         },
+        #     ],
+        #     "message": ""
+        # }
         return test_json
 
     @staticmethod
