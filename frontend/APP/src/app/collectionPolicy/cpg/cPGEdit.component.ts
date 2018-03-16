ecf2bb0e793af5f4655fff7f46890dbe9449b111
necwang <necwang@cisco.com>
2018-03-16 10:56:55 +0800

    20180316 necwang Debug for time intervals changed

--- frontend/APP/src/app/collectionPolicy/cpg/cPGEdit.component.ts
+++ frontend/APP/src/app/collectionPolicy/cpg/cPGEdit.component.ts
@@ -178,7 +178,7 @@ export class CPGEditComponent implements OnInit {
         }
     }
     public execIntervalChange(selExecInterval: any) {
-        if (selExecInterval === '1') {
+        if (selExecInterval === '60') {
             let snmpNameTmp: any = [];
             _.each(this.cpNames, function (cpName) {
                 if (cpName.policy_type.toString() === '1') {
@@ -358,15 +358,15 @@ export class CPGEditComponent implements OnInit {
         $('#moreInfoTable').jqGrid({ searchOnEnter: true, defaultSearch: 'cn' });
     }
     public execIntervalFomatter(id: any) {
-        if (id.toString() === '1') {
+        if (id.toString() === '60') {
             return '1分';
-        } else if (id.toString() === '2') {
+        } else if (id.toString() === '300') {
             return '5分';
-        } else if (id.toString() === '3') {
+        } else if (id.toString() === '900') {
             return '15分';
-        } else if (id.toString() === '4') {
+        } else if (id.toString() === '3600') {
             return '1時間';
-        } else if (id.toString() === '5') {
+        } else if (id.toString() === '86400') {
             return '1日';
         } else {
             return 'null';
