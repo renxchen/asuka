3c917396d54afcb439187230241ecadbbd534aa7
necwang <necwang@cisco.com>
2018-04-03 17:24:05 +0800

    20180403 necwang Update test page and change request server to 134

--- frontend/APP/src/app/testModule/testTelnet.component.ts
+++ frontend/APP/src/app/testModule/testTelnet.component.ts
@@ -27,7 +27,7 @@ export class TestTelnetComponent implements OnInit, AfterViewInit {
 
     }
     submitCli(){
-        let url_cli = 'http://10.79.148.107:1111/v1/api_cli_collection_test/';
+        let url_cli = 'http://10.71.244.134:1111/v1/api_cli_collection_test/';
         // this.testTelnetService.sendRequest(eval("("+this.cliParameter1+")"))
         //       .subscribe(res => {
         //        console.log(res);
@@ -54,13 +54,29 @@ export class TestTelnetComponent implements OnInit, AfterViewInit {
             url: url_cli,
             type: 'post',
             dataType: 'json',
-            data: {'device_info': eval("("+this.cliParameter1+")")}
+            data: {"device_info": this.cliParameter1}
+            // data: {"device_info": eval("("+this.cliParameter1+")")}
           }).done(function (res) {
+              console.log("done");
               console.log(res);
               $('#result1').html(res["data"]["data"]);
               $('#result2').html(res["data"]["log"]);
 
           });
+
+        // $.ajax({
+        //     "type" : 'post',
+        //     "url" : url_cli,
+        //     "dataType" : "json",
+        //     "data" : {"device_info": this.cliParameter1},
+        //     "success" : function(resp) {
+        //         console.log("done");
+        //         console.log(resp);
+        //     },
+        //     "error":function(emsg){
+        //         //返回失败信息-----》emsg
+        //     }
+        // });
 
     }
 
