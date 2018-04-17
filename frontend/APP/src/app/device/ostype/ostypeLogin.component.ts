2716c1e8aecea8c0fa7dcf08d7c2859ba200dc4c
Dan Lv -X (danlv - Millennium at Cisco) <danlv@cisco.com>
2018-04-17 15:11:10 +0800

    20180417 #1480 danlv Fix OS type name input half-width verification

--- frontend/APP/src/app/device/ostype/ostypeLogin.component.ts
+++ frontend/APP/src/app/device/ostype/ostypeLogin.component.ts
@@ -238,7 +238,7 @@ export class OstypeLoginComponent implements OnInit, AfterViewInit {
         this.uniqueFlg = true;
         this.nameNotNull = Validator.notNullCheck(this.name);
         if (this.nameNotNull) {
-            this.nameFlg = Validator.fullWithoutSpecial(this.name) && this.name.length < 31;
+            this.nameFlg = Validator.halfWithoutSpecial(this.name);
         }
         this.telPromptNotNull = Validator.notNullCheck(this.telPrompt);
         if (this.telPromptNotNull) {
